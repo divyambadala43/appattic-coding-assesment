ca941c6d53a95671fdf9f22678bbb494e8f452cb
Divya <divyambadala43@gmail.com>
2022-03-22 00:19:44 +0530

    ADD: Image generator functionality

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const textToImage = require("text-to-image");
+
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const app = express();
 
@@ -14,12 +16,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/post", (req, res) => {
-  res.send("Post");
+  res.send(req.body);
 });
 
-app.post("/post", urlencodedParser, (req, res) => {
+app.post("/post", urlencodedParser, async (req, res) => {
   console.log(req.body);
-  res.render("post");
+  const { colorPicked, quote } = req.body;
+  console.log(colorPicked);
+  const dataUri = await textToImage.generate(quote, {
+    debug: true,
+    maxWidth: 1000,
+    customHeight: 500,
+    fontSize: 18,
+    fontFamily: "Arial",
+    lineHeight: 30,
+    margin: 5,
+    bgColor: "black",
+    textColor: req.body.colorPicked,
+  });
+  res.send(dataUri);
 });
 
 app.listen(port, () => {
